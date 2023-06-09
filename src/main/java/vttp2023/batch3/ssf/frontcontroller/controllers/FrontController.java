12ec190788aa06153320ab1b79baeb8e62bc68d2
crystaleemj <ceemj_@hotmail.com>
2023-06-09 09:23:54 +0800

    landingpage

--- src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
@@ -1,7 +1,20 @@
 package vttp2023.batch3.ssf.frontcontroller.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class FrontController {
 
 	// TODO: Task 2, Task 3, Task 4, Task 6
-	
+
+	// create controller for displaying landing page
+	@GetMapping
+	public String landingPage(Model model){
+
+		return "view0";
+	}
 }
